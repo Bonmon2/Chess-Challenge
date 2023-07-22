3a2eb70cd062b154c61282696f898b4574eb75e1
unknown <benjaminwmoore91@gmail.com>
2023-07-22 15:50:45 +0100

    Update MyBot.cs

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,36 @@
 ﻿using ChessChallenge.API;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
 
 public class MyBot : IChessBot
 {
+    private struct RatedMove 
+    {
+        public RatedMove() { }
+
+        public Move move = Move.NullMove;
+        public int rating = 0;
+    }
+
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
+        foreach (Move move in moves)
+        {
+
+        }
         return moves[0];
+    }
+
+    private RatedMove BasicPawnMove (Move move)
+    {
+        if (move.MovePieceType == (PieceType)1 )
+        {
+            RatedMove thisMove = new RatedMove();
+            thisMove.move = move;
+            thisMove.rating = 10;
+            return thisMove;
+        }
+        return new RatedMove();
     }
 }
